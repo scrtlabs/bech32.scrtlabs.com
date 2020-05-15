de917252a74e442affc1835584cb2520ed5479c3
assafmo <assaf.morami@gmail.com>
2020-05-16 02:34:35 +0300

    add prefix input ✨

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, TextArea } from "semantic-ui-react";
+import { Form, TextArea, Input } from "semantic-ui-react";
 import bech32 from "bech32";
 
 import "semantic-ui-css/semantic.min.css";
@@ -10,8 +10,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       input: "",
-      from: "enigma",
-      to: "secret",
+      from: "",
+      to: "",
     };
   }
 
@@ -19,22 +19,30 @@ class App extends React.Component {
     const placeholder = getPlaceholder(this.state.from);
     return (
       <div>
-        <Form style={{ display: "flex" }}>
+        <Form style={{ display: "flex", height: "8vh" }}>
+          <Input
+            value={this.state.from}
+            placeholder="from: enigma"
+            style={{ padding: "10px" }}
+            onChange={(_, { value }) => this.setState({ from: value })}
+          />
+          <Input
+            value={this.state.to}
+            placeholder="to: secret"
+            style={{ padding: "10px" }}
+            onChange={(_, { value }) => this.setState({ to: value })}
+          />
+        </Form>
+        <Form style={{ display: "flex", height: "92vh" }}>
           <TextArea
-            onChange={(event, data) => {
-              this.setState({ input: data.value });
-            }}
+            onChange={(_, { value }) => this.setState({ input: value })}
             placeholder={placeholder}
-            style={{ height: "100vh" }}
+            style={{ padding: "10px" }}
           />
           <TextArea
-            value={convert(this.state)}
-            placeholder={convert({
-              input: placeholder,
-              from: this.state.from,
-              to: this.state.to,
-            })}
-            style={{ height: "100vh" }}
+            value={convert(this.state.input, this.state.from, this.state.to)}
+            placeholder={convert(placeholder, this.state.from, this.state.to)}
+            style={{ padding: "10px" }}
           />
         </Form>
       </div>
@@ -44,7 +52,10 @@ class App extends React.Component {
 
 const regexCache = {};
 
-function convert({ input, from, to }) {
+function convert(input, from, to) {
+  from = from || "enigma";
+  to = to || "secret";
+
   if (!regexCache[from]) {
     regexCache[from] = new RegExp(
       `${from}(pub|valoper|valoperpub|valcons|valconspub)?1[a-z0-9]+\\b`,
@@ -65,9 +76,11 @@ function convert({ input, from, to }) {
     } catch (error) {
       output = output
         .split(oldAddress)
-        .join(`||| ${oldAddress} ||| <-- ERROR PARSING THIS ADDRESS!`);
+        .join(
+          `||| ${oldAddress} ||| <-- ERROR CONVERTING THIS ADDRESS: ${error.message}`
+        );
 
-      console.error(error.message);
+      console.error(error);
     }
   }
 
@@ -75,33 +88,39 @@ function convert({ input, from, to }) {
 }
 
 function getPlaceholder(from) {
-  return JSON.stringify(
-    {
-      wallet: bech32.encode(
-        from,
-        bech32.decode("enigma1pnsceh64jyrsfwjd2k865eetmsgg5grw8sma87").words
-      ),
-      valoper: bech32.encode(
-        `${from}valoper`,
-        bech32.decode("enigmavaloper1qx5pppsfrqwlnmxj7prpx8rysxm2u5vzqwv3ly")
-          .words
-      ),
-      pub: bech32.encode(
-        `${from}pub`,
-        bech32.decode(
-          "enigmapub1addwnpepqgauy23vhvvr8uezgczuzh7lj64r9ahd4vsshz5fksezk5lw5k6swjskux6"
-        ).words
-      ),
-      valconspub: bech32.encode(
-        `${from}valconspub`,
-        bech32.decode(
-          "enigmavalconspub1zcjduepqj7ygd0gulz2qa03hgzf3ye40pmeyen2z64xjvpkw8mfhuu7j2vcqk6lgcu"
-        ).words
-      ),
-    },
-    null,
-    4
-  );
+  from = from || "enigma";
+
+  try {
+    return JSON.stringify(
+      {
+        wallet: bech32.encode(
+          from,
+          bech32.decode("enigma1pnsceh64jyrsfwjd2k865eetmsgg5grw8sma87").words
+        ),
+        valoper: bech32.encode(
+          `${from}valoper`,
+          bech32.decode("enigmavaloper1qx5pppsfrqwlnmxj7prpx8rysxm2u5vzqwv3ly")
+            .words
+        ),
+        pub: bech32.encode(
+          `${from}pub`,
+          bech32.decode(
+            "enigmapub1addwnpepqgauy23vhvvr8uezgczuzh7lj64r9ahd4vsshz5fksezk5lw5k6swjskux6"
+          ).words
+        ),
+        valconspub: bech32.encode(
+          `${from}valconspub`,
+          bech32.decode(
+            "enigmavalconspub1zcjduepqj7ygd0gulz2qa03hgzf3ye40pmeyen2z64xjvpkw8mfhuu7j2vcqk6lgcu"
+          ).words
+        ),
+      },
+      null,
+      4
+    );
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default App;
